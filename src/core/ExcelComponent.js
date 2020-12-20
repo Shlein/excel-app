b4a018c4a1e7b9e4abdc1ac7848aa364cd7fd428
shlein <optime15022013@mail.ru>
2020-12-20 20:41:54 +0700

    finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,6 +4,15 @@ export class ExcelComponent extends DomListener {
     constructor($root, options = {}) {
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribers = []
+
+        this.prepare()
+    }
+
+    // настраивает компонент до init
+    prepare() {
+
     }
 
     // Возвращает шаблон компонента
@@ -11,11 +20,25 @@ export class ExcelComponent extends DomListener {
         return ''
     }
 
+    // уведомление подписчиков о событии event
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args)
+    }
+
+    // подписка на event
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
+    }
+
+    // инициализируем компонент + дом слушатели
     init() {
         this.initDOMListeners()
     }
 
+    // удаляем компонент + чистим подписчиков
     destroy() {
         this.removeDOMListeners()
+        this.unsubscribers.forEach(unsub => unsub())
     }
 }
